c9411d24e0e4d0d668cb39237d900550a371d74d
alexlenk <alexlenk.ru@gmail.com>
2020-07-27 20:11:56 +0300

    Created mobile version

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,36 @@
 //= vendors/jquery.min.js
 //= vendors/slick/slick.min.js
 
-$('.carousel-section__news').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1
+$(document).ready(function () {
+    $('.carousel-section__news').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 1269,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }]
+    });
+
+    $('.menu-toggle').click(function () {
+        $('html').toggleClass('menu-opened');
+    });
+});
+
+let navMenu = $('.nav-menu');
+
+$(document).mouseup(function (e) {
+    if (navMenu.is(e.target) && navMenu.has(e.target).length === 0) {
+        $('html').removeClass('menu-opened')
+    }
 });
